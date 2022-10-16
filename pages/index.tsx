13eb17043317731512995e76b00a2cc40032cf2b
DongHun, Kim <donghun.kdh@gmail.com>
2022-10-16 20:15:05 +0900

    ADD: setting for mui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@mui/material';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button variant='contained'>TEST</Button>
+    </div>
+  );
 };
 
 export default Home;
