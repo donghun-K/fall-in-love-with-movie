416eb591b5a014b8f66df6f34c139263912a2aa8
DongHun, Kim <donghun.kdh@gmail.com>
2022-11-12 00:42:45 +0900

    FIX: refresh request error

--- pages/api/detail.ts
+++ pages/api/detail.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 async function handler(req: any, res: any) {
   const { movieId } = req.body;
+  if (movieId === undefined) {
+    return;
+  }
   const response = await axios({
     url: `https://api.themoviedb.org/3//movie/${movieId}?api_key=${process.env.API_KEY}&language=ko-KR`,
     method: 'get',
--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 async function handler(req: any, res: any) {
   const { query } = req.body;
+  if (query === undefined) {
+    return;
+  }
   const response = await axios({
     url: `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=ko-KR&page=1&include_adult=true&query=${query}`,
     method: 'get',
