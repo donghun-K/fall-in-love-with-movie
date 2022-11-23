a1d9e168fc1000fbc22704314043d4615b6bc984
DongHun, Kim <donghun.kdh@gmail.com>
2022-11-24 02:49:55 +0900

    CHORE: modify comment section code

--- pages/detail/[slug].tsx
+++ pages/detail/[slug].tsx
@@ -144,189 +144,191 @@ const DetailPage = (props: { data: string }) => {
         <title>{movieDetail?.title}</title>
       </Head>
       {movieDetail !== undefined ? (
-        <Grid
-          container
-          spacing={0}
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            mt: isDownMd ? '1rem' : '5rem',
-          }}
-        >
+        <>
           <Grid
-            item
-            xs={10}
-            sm={8}
-            md={6}
-            lg={5}
+            container
+            spacing={0}
             sx={{
               display: 'flex',
-              justifyContent: isDownMd ? 'center' : 'flex-start',
-              mb: '1rem',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              mt: isDownMd ? '1rem' : '5rem',
             }}
           >
-            <Box
-              sx={{
-                width: isDownMd ? '324px' : '288px',
-                height: isDownMd ? '450px' : '400px',
-                backgroundColor: 'white',
-                boxShadow: '0 0 .3rem .3rem white',
-              }}
-            >
-              <Image
-                src={`https://image.tmdb.org/t/p/original/${movieDetail.poster_path}`}
-                width={isDownMd ? 324 : 288}
-                height={isDownMd ? 450 : 400}
-                alt={movieDetail.title}
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={10} sm={8} md={6} lg={7}>
-            <Box
+            <Grid
+              item
+              xs={10}
+              sm={8}
+              md={6}
+              lg={5}
               sx={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: isDownSm ? 'center' : 'space-between',
-                alignItems: isDownMd ? 'center' : 'flex-start',
-                height: isDownMd ? '300px' : '400px',
+                justifyContent: isDownMd ? 'center' : 'flex-start',
+                mb: '1rem',
               }}
             >
               <Box
                 sx={{
+                  width: isDownMd ? '324px' : '288px',
+                  height: isDownMd ? '450px' : '400px',
+                  backgroundColor: 'white',
+                  boxShadow: '0 0 .3rem .3rem white',
+                }}
+              >
+                <Image
+                  src={`https://image.tmdb.org/t/p/original/${movieDetail.poster_path}`}
+                  width={isDownMd ? 324 : 288}
+                  height={isDownMd ? 450 : 400}
+                  alt={movieDetail.title}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={10} sm={8} md={6} lg={7}>
+              <Box
+                sx={{
                   display: 'flex',
                   flexDirection: 'column',
+                  justifyContent: isDownSm ? 'center' : 'space-between',
                   alignItems: isDownMd ? 'center' : 'flex-start',
+                  height: isDownMd ? '300px' : '400px',
                 }}
               >
-                <Typography
-                  variant='h3'
-                  mt={1}
-                  mb={1}
-                  color='primary'
+                <Box
                   sx={{
-                    fontSize: isDownMd
-                      ? isDownSm
-                        ? '1.3rem'
-                        : '1.8rem'
-                      : '2rem',
-                    color: 'white',
-                    fontWeight: 'bolder',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: isDownMd ? 'center' : 'flex-start',
                   }}
                 >
-                  {movieDetail.title}
-                </Typography>
+                  <Typography
+                    variant='h3'
+                    mt={1}
+                    mb={1}
+                    color='primary'
+                    sx={{
+                      fontSize: isDownMd
+                        ? isDownSm
+                          ? '1.3rem'
+                          : '1.8rem'
+                        : '2rem',
+                      color: 'white',
+                      fontWeight: 'bolder',
+                    }}
+                  >
+                    {movieDetail.title}
+                  </Typography>
 
-                <Typography
-                  variant='subtitle1'
-                  color='primary'
+                  <Typography
+                    variant='subtitle1'
+                    color='primary'
+                    sx={{
+                      fontSize: isDownMd ? '.7rem' : '.8rem',
+                      color: 'lightgray',
+                      fontWeight: 'bolder',
+                    }}
+                  >
+                    {movieDetail.release_date.match(/[0-9]{4}/)} |{' '}
+                    {movieDetail.runtime} min |{' '}
+                    {movieDetail.genres.map((item) => item.name).join(', ')}
+                  </Typography>
+                </Box>
+                <Box
                   sx={{
-                    fontSize: isDownMd ? '.7rem' : '.8rem',
-                    color: 'lightgray',
-                    fontWeight: 'bolder',
-                  }}
-                >
-                  {movieDetail.release_date.match(/[0-9]{4}/)} |{' '}
-                  {movieDetail.runtime} min |{' '}
-                  {movieDetail.genres.map((item) => item.name).join(', ')}
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  height: isDownSm ? '120px' : '200px',
-                  overflow: 'auto',
-                  margin: isDownSm ? '1rem 0' : 0,
-                  '&::-webkit-scrollbar': {
-                    width: '10px',
-                  },
-                  '&::-webkit-scrollbar-thumb': {
-                    background: 'lightgray',
-                    backgroundClip: 'padding-box',
-                    border: '2px solid transparent',
-                    borderRadius: '10px',
-                  },
-                  '&::-webkit-scrollbar-track': {
-                    backgroundColor: '#111111',
-                    borderRadius: '10px',
-                  },
-                }}
-              >
-                <Typography
-                  variant='body1'
-                  mt={isDownSm ? 1 : 0}
-                  mb={isDownSm ? 1 : 0}
-                  sx={{
-                    fontSize: isDownMd ? '.8rem' : '.9rem',
-                    color: 'lightgray',
-                    maxHeight: '100%',
-                  }}
-                >
-                  {movieDetail.overview}
-                </Typography>
-              </Box>
-
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: isDownSm ? 'column-reverse' : 'row',
-                  justifyContent: 'flex-start',
-                  alignItems: 'center',
-                }}
-              >
-                <Rating
-                  sx={{
-                    fontSize: isDownSm ? '2.5rem' : '2.8rem',
-                    '& .MuiRating-icon': {
-                      color: (theme) => theme.palette.primary.main,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    height: isDownSm ? '120px' : '200px',
+                    overflow: 'auto',
+                    margin: isDownSm ? '1rem 0' : 0,
+                    '&::-webkit-scrollbar': {
+                      width: '10px',
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                      background: 'lightgray',
+                      backgroundClip: 'padding-box',
+                      border: '2px solid transparent',
+                      borderRadius: '10px',
+                    },
+                    '&::-webkit-scrollbar-track': {
+                      backgroundColor: '#111111',
+                      borderRadius: '10px',
                     },
                   }}
-                  value={rating === undefined ? null : rating}
-                  onChange={(e, value) => {
-                    if (status === 'unauthenticated') {
-                      alert('로그인 먼저 해주세요!');
-                      return;
-                    }
-                    if (rating === value) {
-                      return;
-                    }
-                    if (value !== null) {
-                      setRating(value);
-                    } else {
-                      setRating(null);
-                    }
-                    postRating({
-                      username: session?.user?.name,
-                      movieCode: props.data,
-                      rating: value,
-                    });
-                  }}
-                />
-                <Typography
-                  ml={isDownSm ? 0 : 2}
+                >
+                  <Typography
+                    variant='body1'
+                    mt={isDownSm ? 1 : 0}
+                    mb={isDownSm ? 1 : 0}
+                    sx={{
+                      fontSize: isDownMd ? '.8rem' : '.9rem',
+                      color: 'lightgray',
+                      maxHeight: '100%',
+                    }}
+                  >
+                    {movieDetail.overview}
+                  </Typography>
+                </Box>
+
+                <Box
                   sx={{
-                    color: 'white',
-                    fontWeight: 'bolder',
-                    fontSize: isDownSm ? '1rem' : '1.3rem',
+                    display: 'flex',
+                    flexDirection: isDownSm ? 'column-reverse' : 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
                   }}
                 >
-                  {rating !== null && rating !== undefined
-                    ? `${rating} / 5`
-                    : '평가하기!'}
-                </Typography>
+                  <Rating
+                    sx={{
+                      fontSize: isDownSm ? '2.5rem' : '2.8rem',
+                      '& .MuiRating-icon': {
+                        color: (theme) => theme.palette.primary.main,
+                      },
+                    }}
+                    value={rating === undefined ? null : rating}
+                    onChange={(e, value) => {
+                      if (status === 'unauthenticated') {
+                        alert('로그인 먼저 해주세요!');
+                        return;
+                      }
+                      if (rating === value) {
+                        return;
+                      }
+                      if (value !== null) {
+                        setRating(value);
+                      } else {
+                        setRating(null);
+                      }
+                      postRating({
+                        username: session?.user?.name,
+                        movieCode: props.data,
+                        rating: value,
+                      });
+                    }}
+                  />
+                  <Typography
+                    ml={isDownSm ? 0 : 2}
+                    sx={{
+                      color: 'white',
+                      fontWeight: 'bolder',
+                      fontSize: isDownSm ? '1rem' : '1.3rem',
+                    }}
+                  >
+                    {rating !== null && rating !== undefined
+                      ? `${rating} / 5`
+                      : '평가하기!'}
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
+            </Grid>
           </Grid>
-        </Grid>
+          <CommentsSection
+            isDownMd={isDownMd}
+            isDownSm={isDownSm}
+            username={session?.user?.name as string}
+            movieCode={props.data}
+          />
+        </>
       ) : null}
-      <CommentsSection
-        isDownMd={isDownMd}
-        isDownSm={isDownSm}
-        username={session?.user?.name as string}
-        movieCode={props.data}
-      />
     </Box>
   );
 };
